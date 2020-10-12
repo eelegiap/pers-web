de3b339a0ed8ce6c1065b69209960d88a227fa9c
Paige Lee <paigelee@Paiges-MacBook-2.local>
2020-10-12 02:34:06 -0400

    substantial changes to the website

--- menu.js
+++ menu.js
@@ -3,6 +3,8 @@
   
     $(function () {
       $(".menu-container").load("menu.html");
+      $(".links").load("links.html");
+      $(".footer").load("footer.html");
       $(".fade").delay(5).animate({"opacity": "1"}, 500);
     });
   })();
